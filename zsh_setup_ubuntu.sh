c1f67a6cdf4acf853606e09996e3cb95174a7dda
Nicholas A. DiPasquale <ndipasquale@homeaway.com>
2017-08-08 09:24:33 -0500

    Initial commit

---
+++ zsh_setup_ubuntu.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+apt-get install zsh
+apt-get install git-core 
+
+wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
+
+chsh -s `which zsh`
+
+shutdown -r 0
